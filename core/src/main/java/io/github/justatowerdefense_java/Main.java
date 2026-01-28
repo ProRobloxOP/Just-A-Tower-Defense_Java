e5627be8eed971f84bd3e4f5ec40befcfc670e26
TysonYung <tysony@nycstudents.net>
2026-01-28 11:02:56 -0500

    Added some Javadoc Comments

--- core/src/main/java/io/github/justatowerdefense_java/Main.java
+++ core/src/main/java/io/github/justatowerdefense_java/Main.java
@@ -4,16 +4,33 @@ import Modules.Utilities.Screens.Loader;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.utils.viewport.*;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * The class that is called by libGDX to run the game. Controls the mainViewport and creates the main Loader Handle.
+ *
+ * @author Tyson Yung
+ */
 public class Main extends Game {
+    /**
+     * Stores the mainViewport which can be used as a frame to hold the main window settings.
+     */
     public StretchViewport mainViewpoint;
 
+    /**
+     * LibGDX calls this method, which creates the mainViewport instance.
+     * <p>Also creates the main Loader and sets the screen to it.</p>
+     */
     @Override
     public void create() {
         mainViewpoint = new StretchViewport(960, 540);
         this.setScreen(new Loader(this));
     }
 
+    /**
+     *
+     *
+     * @param width given width to resize by LibGDX
+     * @param height given height to resize by LibGDX
+     */
     @Override
     public void resize(int width, int height) {
         super.resize(width, height);
